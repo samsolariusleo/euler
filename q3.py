72688506ce25083ccb4d3aa2e8b3b63910fd3f92
samsolariusleo <gan.jingying@dhs.sg>
2014-02-21 10:46:42 +0800

    Update q3.py
    Needs review

--- q3.py
+++ q3.py
@@ -1,31 +1,36 @@
 import math
 
 def primefactors(num):
+    primelimit = "{0:.0f}".format(math.sqrt(num))
+    primelimit = int(primelimit)
     factor = 2
     found = False
 
     # find smallest factor
-    while factor < num and not found:
+    while factor < primelimit and not found:
         if num % factor == 0:
             # find the larger factor
             largest = num // factor
-
+            
             # test if the number is prime
             if isprime(largest):
-                 found = True
-                 break
+                found = True
+                return largest
             else:
                 factor = factor + 1
         else:
             factor = factor + 1
-            
-    return largest
+    return "Not found."
 
 def isprime(num):
+    primelimit = "{0:.0f}".format(math.sqrt(num))
+    primelimit = int(primelimit)
+    
     prime = True
-    for i in range(2,num-1):
+    for i in range(2,primelimit):
         if num % i == 0:
             prime = False
     return prime
 
+print(primefactors(13195))
 print(primefactors(600851475143))
